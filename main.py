50c5d7affabdc244fc63317ceccc8ac0392024b1
Sharad Dahal <83944190+dahalsharad@users.noreply.github.com>
2022-08-14 15:19:31 +0545

    files added

---
+++ main.py
@@ -0,0 +1,62 @@
+import pygame, sys, s0rt
+from button import Button
+
+pygame.init()
+
+SCREEN = pygame.display.set_mode((1280, 720))
+pygame.display.set_caption("Menu")
+
+BG = pygame.image.load("assets/Background.png")
+
+def get_font(size): # Returns Press-Start-2P in the desired size
+    return pygame.font.Font("assets/font.ttf", size)
+
+def play():
+    while True:
+       import dij
+
+def options():
+    while True:
+        import astar    
+    
+def sort():
+    while True:
+        s0rt.main()
+
+        
+
+def main_menu():
+    while True:
+        SCREEN.blit(BG, (0, 0))
+
+        MENU_MOUSE_POS = pygame.mouse.get_pos()
+
+        MENU_TEXT = get_font(100).render("MAIN MENU", True, "#b68f40")
+        MENU_RECT = MENU_TEXT.get_rect(center=(640, 100))
+
+        PLAY_BUTTON = Button(image=pygame.image.load("assets/Play Rect.png"), pos=(640, 250), 
+                            text_input="DIJ", font=get_font(75), base_color="#d7fcd4", hovering_color="White")
+        OPTIONS_BUTTON = Button(image=pygame.image.load("assets/Options Rect.png"), pos=(640, 400), 
+                            text_input="ASTAR", font=get_font(75), base_color="#d7fcd4", hovering_color="White")
+        SORT_BUTTON = Button(image=pygame.image.load("assets/Play Rect.png"), pos=(640, 550), 
+                            text_input="SORT", font=get_font(75), base_color="#d7fcd4", hovering_color="White")
+
+        SCREEN.blit(MENU_TEXT, MENU_RECT)
+
+        for button in [PLAY_BUTTON, OPTIONS_BUTTON, SORT_BUTTON]:
+            button.changeColor(MENU_MOUSE_POS)
+            button.update(SCREEN)
+        
+        for event in pygame.event.get():
+            
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if PLAY_BUTTON.checkForInput(MENU_MOUSE_POS):
+                    play()
+                if OPTIONS_BUTTON.checkForInput(MENU_MOUSE_POS):
+                    options()
+                if SORT_BUTTON.checkForInput(MENU_MOUSE_POS):
+                    sort()
+
+        pygame.display.update()
+
+main_menu()
